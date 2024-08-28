9bfb3b915914e16bd63555ffec27e65c6dacaf73
danbi <4679jeong@naver.com>
2024-08-28 22:19:58 +0900

    0828

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  // const myKey = process.env.REACT_APP_API_KEY;
-  // console.log(myKey)
+ 
+  //날씨
   type weathers = {id: number; temp: number; main: string; icon: string}
   const [weather, setWeather] = useState<weathers>();
 
@@ -14,23 +14,33 @@ function App() {
       const datas = res.data;
       setWeather({
         id : datas.weather[0].id,
-        temp : datas.main.temp,
+        temp : Math.round(datas.main.temp - 273.15),
         main: datas.weather[0].main,
         icon: datas.weather[0].icon
       })
     })
   }
-  console.log(weather)
   const weatherIcon = `https://openweathermap.org/img/wn/${weather?.icon}.png`;
+
+  //날짜, 시간
+  const date = new Date();
+  const day = date.getDay();
+  const dayList = ['Sun', 'Mon', 'Tue', 'Wed', 'The', 'Fri', 'Sat'];
+  const today = dayList[day];
+  const hour =(date.getHours()<10? '0' + date.getHours() : date.getHours());
+  const minute = (date.getMinutes()<10? '0' + date.getMinutes() : date.getMinutes());
+  const timeNow = `${hour}:${minute}`;
+
   useEffect(()=>{getWeather()},[])
 
 
   return (
     <div className="App">
-     <div>오늘의 서울 날씨 입니다.</div>
-     <div>{weather?.main}</div>
+     <h2>SEOUL</h2>
+     <h3>{today} {timeNow}</h3>
+     <h4>{weather?.temp}°</h4>
      <div><img src={weatherIcon} alt="날씨 아이콘" /></div>
-     <div>{weather?.temp}</div>
+     <h5>{weather?.main}</h5>
     </div>
   );
 }
