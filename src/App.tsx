b7278a064b242aad95e2ea74d23552c287e12de8
danbi <4679jeong@naver.com>
2024-09-02 20:55:29 +0900

    0902

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
+import { useEffect, useRef } from 'react';
 import './App.scss';
 import Times from './assets/component/times';
 import Weathers from './assets/component/weathers';
 
 function App() {
  
+  //배경색
+  const bgColor = useRef<HTMLDivElement>(null);
+  
+  function bg (){
+    const bgDate = new Date();
+    const bgHour = bgDate.getHours();
+
+    if(bgHour<=18){
+      if(bgColor.current !== null){
+        bgColor.current.style.backgroundColor = "#FF8C00"; 
+      }
+    }else{
+      if(bgColor.current !== null){
+        bgColor.current.style.backgroundColor = "#191970"; 
+      }
+    }
+  }
+
+  useEffect(()=>{bg()},[bg])
+
 
 
   return (
     <div className="App">
-     <Times/>
-     <Weathers/>
+      <div className="cont" ref={bgColor}>
+      <Times/>
+      <Weathers/>
+      </div>
     </div>
   );
 }
